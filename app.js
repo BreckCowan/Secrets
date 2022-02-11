11114439de8ec0b1be4348fd0e2f91179c38092e
Breck Cowan <breckcowan@gmail.com>
2022-02-11 14:03:49 -0600

    added secrets to .env

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
-const dotenv = require("dotenv/config");
-
-// const port = 3000;
 
 const app = express();
 
@@ -18,10 +16,16 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
 });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+
+// console.log(process.env.SECRET);
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
 
 const User = new mongoose.model("User", userSchema);
 
